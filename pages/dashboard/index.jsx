7dc8addce01b592236000940bafa40151d4f720a
KhresnaDeva <khresnadeva23@gmail.com>
2023-08-25 11:13:04 +0700

    update

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,10 +1,11 @@
+'use'
 import { useRouter } from "next/router";
 
-function dashboardpage (){
+function Dashboardpage (){
     const router = useRouter()
     const dashboard = router.query.dashboard
 
     return <h1>ini dashboard {dashboard}</h1>
 }
 
-export default dashboardpage
+export default Dashboardpage
