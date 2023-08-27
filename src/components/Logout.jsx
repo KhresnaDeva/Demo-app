b7e412a7024430b5d206b69ca253fd9ab6ff66c8
KhresnaDeva <khresnadeva23@gmail.com>
2023-08-27 18:09:06 +0700

    fixing logout button

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -15,7 +15,11 @@ const Logout = ({iam = null}) => {
               role="link"
               type="button"
               className="block bg-red-500 px-4 py-2 font-bold hover:bg-red-600 text-sm rounded-lg"
-              onClick={() => {window.location.assign(iam.logoutURL())}}>
+              onClick={() => {
+                window.localStorage.removeItem("access_token")
+                window.location.assign(iam.logoutURL())
+                window.location.href = "/"
+                }}>
               LOGOUT
             </button>
           </div>
